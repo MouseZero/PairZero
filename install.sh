545b9464f4498d742b07de7a1b5186d9a3c068bb
codelogicws <russ@codelogic.ws>
2016-06-25 16:49:47 -0700

    Fixed path

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ echo source ~/.pairConfig/vimConfig/vimrc.local >> ~/.vimrc.local
 echo source ~/.pairConfig/vimConfig/vimrc.before.local >> ~/.vimrc.before.local
 echo source ~/.pairConfig/vimConfig/vimrc.bundles.local >> ~/.vimrc.bundles.local
 echo source "~/.pairConfig/tmuxConfig/tmux.conf" >> ~/.tmux.conf
-cp ./tmuxConfig/Bable.yml ~/.tmuxinator/Bable.yml
+cp ~/.pairConfig/tmuxConfig/Bable.yml ~/.tmuxinator/Bable.yml
