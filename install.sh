357c8c76984aa0e98b5a53f52a4f95b9b8ffdecc
codelogicws <russ@codelogic.ws>
2016-06-25 16:26:51 -0700

    First commit from this repo

---
+++ install.sh
@@ -0,0 +1,6 @@
+echo . ~/.pairConfig/scripts/bashPairConfig >> ~/.bashrc
+echo source ~/.pairConfig/vimConfig/vimrc.local >> ~/.vimrc.local
+echo source ~/.pairConfig/vimConfig/vimrc.before.local >> ~/.vimrc.before.local
+echo source ~/.pairConfig/vimConfig/vimrc.bundles.local >> ~/.vimrc.bundles.local
+echo source "~/.pairConfig/tmuxConfig/tmux.conf" >> ~/.tmux.conf
+cp ./tmuxConfig/Bable.yml ~/.tmuxinator/Bable.yml
