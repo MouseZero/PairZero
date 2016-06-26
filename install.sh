74ca9fcea0ac0b46fc21beba1e4fda583569890e
codelogicws <russ@codelogic.ws>
2016-06-26 13:58:06 -0700

    Writing to profile instead of bash RC

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-echo . ~/.pairConfig/scripts/bashPairConfig >> ~/.bashrc
+echo . ~/.pairConfig/scripts/bashPairConfig >> ~/.profile
 echo source ~/.pairConfig/vimConfig/vimrc.local >> ~/.vimrc.local
 echo source ~/.pairConfig/vimConfig/vimrc.before.local >> ~/.vimrc.before.local
 echo source ~/.pairConfig/vimConfig/vimrc.bundles.local >> ~/.vimrc.bundles.local
