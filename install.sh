3adae3065bcffa4f67c2aa449f933de8105afe8a
codelogicws <russ@codelogic.ws>
2016-06-26 15:18:26 -0700

    Adding a demo folder for demo code

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@ echo source ~/.pairConfig/vimConfig/vimrc.local >> ~/.vimrc.local
 echo source ~/.pairConfig/vimConfig/vimrc.before.local >> ~/.vimrc.before.local
 echo source ~/.pairConfig/vimConfig/vimrc.bundles.local >> ~/.vimrc.bundles.local
 echo source "~/.pairConfig/tmuxConfig/tmux.conf" >> ~/.tmux.conf
+mkdir ~/workspace
+mkdir ~/workspace/demo
 cp ~/.pairConfig/tmuxConfig/Bable.yml ~/.tmuxinator/Bable.yml
 vim +PluginInstall +qall
