1bf02e8f878a09afcf21644afbb93fba63d5970c
codelogicws <russ@codelogic.ws>
2016-06-26 10:05:06 -0700

    Vim now updates its plugins

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ echo source ~/.pairConfig/vimConfig/vimrc.before.local >> ~/.vimrc.before.local
 echo source ~/.pairConfig/vimConfig/vimrc.bundles.local >> ~/.vimrc.bundles.local
 echo source "~/.pairConfig/tmuxConfig/tmux.conf" >> ~/.tmux.conf
 cp ~/.pairConfig/tmuxConfig/Bable.yml ~/.tmuxinator/Bable.yml
+vim +PluginInstall +qall
